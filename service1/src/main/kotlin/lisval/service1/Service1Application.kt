338d5c30584ef12adba995cf22f7a8a1f9494be8
neonik <lis.gleb2012@gmail.com>
2025-12-20 07:22:44 +0300

    add hyperoxy and Consul for service1

--- service1/src/main/kotlin/lisval/service1/Service1Application.kt
+++ service1/src/main/kotlin/lisval/service1/Service1Application.kt
@@ -2,10 +2,12 @@ package lisval.service1
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 class Service1Application
 
 fun main(args: Array<String>) {
